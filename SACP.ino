64d636e63446146998bf90974743610bbd92d381
MarcinsaS <msanche1@insa-toulouse.fr>
2024-11-27 10:08:33 +0100

    fin des test

--- SACP.ino
+++ SACP.ino
@@ -1,14 +1,36 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "SHT31.h"
+#include <math.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
 
 SHT31 sht31 = SHT31();
+
+const int ledPin=6;                 //Connect the LED Grove module to Pin12, Digital 12
+const int thresholdvalue=10;         //The threshold for which the LED should turn on. 
+float Rsensor; //Resistance of sensor in K
 
 void setup() {
   Serial.begin(9600);
   while(!Serial);
   Serial.println("begin...");  
   sht31.begin();
+  
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+    
+  lcd.setRGB(colorR, colorG, colorB);
+    
+  // Print a message to the LCD.
+  //lcd.print("hello, world!");
+
+  delay(1000);
 }
 
 void loop() {
@@ -21,6 +43,27 @@ void loop() {
   Serial.print(hum);
   Serial.println("%"); 
   Serial.println();
+
+  int sensorValue = analogRead(0); 
+  Rsensor=(float)(1023-sensorValue)*10/sensorValue;
+
+  float lux=exp(float(sensorValue)/100.0);
+  //float lux=log(Rsensor);
+  Serial.println("the analog read data is ");
+  Serial.println(lux);
+  Serial.println("the sensor resistance is ");
+  Serial.println(Rsensor,DEC);//show the light intensity on the serial monitor;
   delay(1000);
+  if (Serial.available()) {
+        // wait a bit for the entire message to arrive
+        delay(100);
+        // clear the screen
+        lcd.clear();
+        // read all the available characters
+        while (Serial.available() > 0) {
+            // display each character to the LCD
+            lcd.write(Serial.read());
+        }
+    }
 
 }
