2bca080ebfaa60e221de02e4522df26a5dbb6c77
MarcinsaS <msanche1@insa-toulouse.fr>
2024-11-28 10:01:22 +0100

    class

---
+++ Application.cpp
@@ -0,0 +1,98 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "SHT31.h"
+#include <math.h>
+#include <string>
+#include "rgb_lcd.h"
+
+using namespace std;
+
+rgb_lcd lcd;
+
+SHT31 sht31 = SHT31();
+
+class TempHum {
+  float temp;
+  float hum;
+
+  public:
+
+  TempHum(){}
+
+  void Initialize(){
+    sht31.begin();
+    temp=sht31.getTemperature();
+    hum=sht31.getHumidity();
+  }
+
+  float readTemp(){
+    return temp;
+  }
+
+  float readHum(){
+    return hum;
+  }
+
+  void Update(){
+    temp=sht31.getTemperature();
+    hum=sht31.getHumidity();
+  }
+
+
+
+};
+
+class Luminosity {
+
+  float lum;
+  float Rsensor;
+  int aread;
+
+  public:
+
+  Luminosity(){}
+
+  void Initialize(){
+    aread = analogRead(0); 
+    Rsensor=(float)(1024-aread)*10/aread;
+    lum=exp(float(aread)/75.0);
+  }
+
+  float getLum(){
+    return lum;
+  }
+
+  void Update(){
+    aread = analogRead(0); 
+    Rsensor=(float)(1024-aread)*10/aread;
+    lum=exp(float(aread)/75.0);
+  }
+};
+
+class Lcd{
+  bool isOn=0;
+  int colorR = 255;
+  int colorG = 0;
+  int colorB = 0;
+  string data;
+  string error;
+
+  public:
+
+  Lcd();
+
+  void Initialize(){
+    isOn=1;
+    lcd.begin(16, 2);
+    lcd.setRGB(colorR, colorG, colorB);
+  }
+
+  void Update(){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(data);  
+    lcd.setCursor(0, 1);
+    lcd.print(error);  
+  }
+
+};
--- SACP.ino
+++ SACP.ino
@@ -2,7 +2,10 @@
 #include <Wire.h>
 #include "SHT31.h"
 #include <math.h>
+#include <string>
 #include "rgb_lcd.h"
+
+using namespace std;
 
 rgb_lcd lcd;
 
@@ -15,6 +18,93 @@ SHT31 sht31 = SHT31();
 const int ledPin=6;                 //Connect the LED Grove module to Pin12, Digital 12
 const int thresholdvalue=10;         //The threshold for which the LED should turn on. 
 float Rsensor; //Resistance of sensor in K
+
+class TempHum {
+  float temp;
+  float hum;
+
+  public:
+
+  TempHum(){}
+
+  void Initialize(){
+    sht31.begin();
+    temp=sht31.getTemperature();
+    hum=sht31.getHumidity();
+  }
+
+  float readTemp(){
+    return temp;
+  }
+
+  float readHum(){
+    return hum;
+  }
+
+  void Update(){
+    temp=sht31.getTemperature();
+    hum=sht31.getHumidity();
+  }
+
+
+
+};
+
+class Luminosity {
+
+  float lum;
+  float Rsensor;
+  int aread;
+
+  public:
+
+  Luminosity(){}
+
+  void Initialize(){
+    aread = analogRead(0); 
+    Rsensor=(float)(1024-aread)*10/aread;
+    lum=exp(float(aread)/75.0);
+  }
+
+  float getLum(){
+    return lum;
+  }
+
+  void Update(){
+    aread = analogRead(0); 
+    Rsensor=(float)(1024-aread)*10/aread;
+    lum=exp(float(aread)/75.0);
+  }
+};
+
+class Lcd{
+  bool isOn=0;
+  int colorR = 255;
+  int colorG = 0;
+  int colorB = 0;
+  string data;
+  string error;
+
+  public:
+
+  Lcd();
+
+  void Initialize(){
+    isOn=1;
+    lcd.begin(16, 2);
+    lcd.setRGB(colorR, colorG, colorB);
+  }
+
+  void Update(){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(data);  
+    lcd.setCursor(0, 1);
+    lcd.print(error);  
+  }
+
+};
+
 
 void setup() {
   Serial.begin(9600);
@@ -40,11 +130,10 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("Temp = "); 
   lcd.print(temp);
-  lcd.print(" C"); //The unit for  Celsius because original arduino don't support speical symbols
+  lcd.print(" C"); 
   lcd.setCursor(0, 1);
   lcd.print("Hum = "); 
   lcd.print(hum);
-  3
   lcd.print("%"); 
 
   int sensorValue = analogRead(0); 
